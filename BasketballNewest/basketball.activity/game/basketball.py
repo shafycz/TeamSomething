03c1098f8e31d4e6c5f540e87f577ef35db4dd89
Vpapa <Vpapa@udel.edu>
2014-05-09 17:51:53 -0400

    updated animations for question phase

--- BasketballNewest/basketball.activity/game/basketball.py
+++ BasketballNewest/basketball.activity/game/basketball.py
@@ -14,6 +14,8 @@ SIZE = (WIDTH, HEIGHT)
 BASKETX = 0 #x,y pos of the chose basket
 BASKETY = 375
 initX = WIDTH/2 
+dist = 0
+speed = 2.0
 
 
 class One(spyral.Sprite):
@@ -99,12 +101,13 @@ class Basketball(spyral.Scene):
 	if x_array[0] == 0:
 		if pos.x >400 and pos.x<=800 and pos.y>300:
 			BASKETX = 600
+			dist = BASKETX - initX
 			#self.b2.kill()
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
-			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(0,1200)), duration = 4)
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, initX+dist), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
+			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(400,800)), duration = speed)
 			missAnim2 = Animation('y', easing.Linear(BASKETY, 0), duration = 4)
 			
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2)+(missAnim&missAnim2))
@@ -113,11 +116,12 @@ class Basketball(spyral.Scene):
 			i =random.randint(1,2)
 			if i == 1:
 				BASKETX = 145
+				dist = BASKETX - WIDTH
 				print "computer scores on 1"
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, WIDTH+dist), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball1.animate(anim)
@@ -126,12 +130,13 @@ class Basketball(spyral.Scene):
 		elif pos.x>800 and pos.y>300:
 			#self.b3.kill()
 			BASKETX = 1000
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
-			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(0,1200)), duration = 4)
-			missAnim2 = Animation('y', easing.Linear(BASKETY, 0), duration = 4)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, initx+dist), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
+			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(800,1200)), duration = speed)
+			missAnim2 = Animation('y', easing.Linear(BASKETY, 0), duration = speed)
 			
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2)+(missAnim&missAnim2))
 		
@@ -139,11 +144,12 @@ class Basketball(spyral.Scene):
 			i =random.randint(1,2)
 			if i == 1:
 				BASKETX = 145
+				dist = BASKETX - WIDTH
 				print "computer scores on 1"
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, WIDTH+dist), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball1.animate(anim)
@@ -151,10 +157,11 @@ class Basketball(spyral.Scene):
 				spyral.director.replace(subject.Subject())
 		elif pos.x <400 and pos.y>300:
 			BASKETX = 145
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = speed)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, initX+dist), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 			self.ball1.animate(anim)
 			game.Counter = game.Counter +2
@@ -164,12 +171,13 @@ class Basketball(spyral.Scene):
 		if pos.x <400 and pos.y>300:
 			#self.b1.kill()
 			BASKETX = 145
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
-			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(BASKETX-200,BASKETX+200)), duration = 4)
-			missAnim2 = Animation('y', easing.QuadraticIn(BASKETY, HEIGHT), duration = 3)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, initx+dist), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
+			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(BASKETX-200,BASKETX+200)), duration = speed)
+			missAnim2 = Animation('y', easing.QuadraticIn(BASKETY, HEIGHT), duration = speed)
 			
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2)+(missAnim&missAnim2))
 			self.ball1.animate(anim)
@@ -177,10 +185,11 @@ class Basketball(spyral.Scene):
 			if i == 1:
 				print "computer scores on 2"
 				BASKETX = 600
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				dist = BASKETX - WIDTH
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, WIDTH+dist), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball2.animate(anim)
@@ -189,11 +198,12 @@ class Basketball(spyral.Scene):
 		elif pos.x>800 and pos.y>300:
 			#self.b3.kill()
 			BASKETX = 1000
-			xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
-			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(BASKETX-200,BASKETX+200)), duration = 4)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, initX+dist), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
+			missAnim = Animation('x', easing.Linear(BASKETX, math.random.randint(BASKETX-200,BASKETX+200)), duration = speed)
 			missAnim2 = Animation('y', easing.QuadraticIn(BASKETY, HEIGHT), duration = 3)
 			
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2)+(missAnim&missAnim2))
@@ -202,10 +212,11 @@ class Basketball(spyral.Scene):
 			if i == 1:
 				BASKETX = 600
 				print "computer scores on 2"
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				dist = BASKETX - WIDTH
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, BASKETX), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball2.animate(anim)
@@ -213,10 +224,11 @@ class Basketball(spyral.Scene):
 				spyral.director.replace(subject.Subject())
 		elif pos.x >400 and pos.x <800 and pos.y>300:
 			BASKETX = 600
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, BASKETX), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 			self.ball2.animate(anim)
 			game.Counter = game.Counter +2			
@@ -226,10 +238,11 @@ class Basketball(spyral.Scene):
 		if pos.x <400 and pos.y>300:
 			#self.b1.kill()
 			BASKETX = 145
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)-75), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2)-75, BASKETX-75), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+(dist/2)-75), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+(dist/2)-75, BASKETX-75), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))						
 			self.ball1.animate(anim)
@@ -238,10 +251,11 @@ class Basketball(spyral.Scene):
 			if i == 1:
 				BASKETX = 1000
 				print "computer scores on 3"
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				dist = BASKETX - WIDTH
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, BASKETX), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball3.animate(anim)
@@ -250,10 +264,11 @@ class Basketball(spyral.Scene):
 		elif pos.x<800 and pos.x>400 and pos.y>300:
 			#self.b2.kill()
 			BASKETX = 600
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)-75), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2)-75, BASKETX-75), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+(dist/2)-75), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+(dist/2)-75, BASKETX-75), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))			
 
@@ -263,10 +278,11 @@ class Basketball(spyral.Scene):
 			if i == 1:
 				print "computer scores on 3"
 				BASKETX = 1000
-				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH-((WIDTH+BASKETX)/2)), duration = 4.0)
-				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/3, BASKETY-100),duration=2.0)
-				xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((WIDTH+BASKETX)/2), BASKETX), duration = 4.0)
-				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+				dist = BASKETX - WIDTH
+				xCoordAnim = Animation('x', easing.Linear(WIDTH, WIDTH+dist/2), duration = speed)
+				yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT/4, BASKETY-100),duration=speed)
+				xCoordAnim2 = Animation('x', easing.Linear(WIDTH+dist/2, BASKETX), duration = speed)
+				yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 
 				anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 				self.ball3.animate(anim)
@@ -274,10 +290,11 @@ class Basketball(spyral.Scene):
 				spyral.director.replace(subject.Subject())
 		elif pos.x >800 and pos.y>300:
 			BASKETX = 1000
-			xCoordAnim = Animation('x', easing.Linear(initX, WIDTH-((initX+BASKETX)/2)), duration = 4.0)
-			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration=2.0)
-			xCoordAnim2 = Animation('x', easing.Linear(WIDTH-((initX+BASKETX)/2), BASKETX), duration = 4.0)
-			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = 2.0)
+			dist = BASKETX - initX
+			xCoordAnim = Animation('x', easing.Linear(initX, initX+dist/2), duration = speed)
+			yCoordAnim1 = Animation('y', easing.QuadraticOut(HEIGHT, BASKETY-100),duration= speed)
+			xCoordAnim2 = Animation('x', easing.Linear(initX+dist/2, BASKETX), duration = speed)
+			yCoordAnim2 = Animation('y', easing.QuadraticIn(BASKETY-100,BASKETY), duration = speed)
 			anim = ((xCoordAnim & yCoordAnim1)+(xCoordAnim2+yCoordAnim2))
 			self.ball3.animate(anim)
 			
