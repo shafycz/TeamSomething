6b49d132bb4fee3491a9c276bc85ce909de4c5c0
pkrespan <pkrespan@udel.edu>
2014-05-18 19:46:03 -0400

    Update question.py

--- BasketballNewest/basketball.activity/game/question.py
+++ BasketballNewest/basketball.activity/game/question.py
@@ -53,7 +53,7 @@ class Question(spyral.Scene):
         	self.background = spyral.Image(filename= "game/colors/purpleb3.png")
         elif game.Color==5:
         	self.background = spyral.Image(filename= "game/colors/redb3.png")
-        
+ '''       
 	class selectDifficultyForm(spyral.Form) 
 		easy = spyral.widgets.Button("easy", "Easy")
 		medium = spyral.widgets.Button("medium", "Medium")
@@ -97,82 +97,88 @@ class Question(spyral.Scene):
 		#message5 =sont.render(subject_array[0]);
 		#message6 = sont.render(verb_array[0]);
 		#message7 = sont.render(number_array[0]);
-		
+'''
 	spyral.event.register("input.mouse.down.left", self.handle_clicked) 
 	spyral.event.register("system.quit", spyral.director.pop)
 	spyral.event.register("input.keyboard.down.q", spyral.director.pop)
  	font = spyral.Font("/usr/share/fonts/truetype/freefont/FreeMono.ttf",36,(255,255,255))
-	question1_array = ["The cat ___ the bird around the room.", "John is ___ to school.", "You are ___ right now.", "Lions ___ after their prey.",
-	 "Can I ___ to your house?", "People ___ when they are sad.", "An iPhone ___ a cellphone.", "The player ___ the basketball.",
-	  "Beyonce ___ on stage with Jay Z.", "We will ___ in class because it is fun."]
+	question_array = ["The cat ___ the bird around the room.", "John is ___ to school.", "You are ___ right now.", "Lions ___ after their prey.",
+	"Can I ___ to your house?", "People ___ when they are sad.", "An iPhone ___ a cellphone.", "The player ___ the basketball.",
+	"Beyonce ___ on stage with Jay Z.", "We will ___ in class because it is fun.",
 	
 	#Increased difficulty by making sentence structure slightly more complex
-	question2_array=["I ___ March Madness on my television.", "___ a paper can be stressful.", "I ___ my homework to my room.", 
-	"He was ___ daydreaming in class.", "You probably should not be ___ in class.", "The student ___ in his art class yesterday.", "___ up is hard.", 
-	"I ___ The Eagles will win the Superbowl.", "All CCCS students ___ awesome!", "Anthony ___ school early because he was sick."]
+	"I ___ March Madness on my television.", "___ a paper can be stressful.", "I ___ my homework to my room.", 
+	"He was ___ daydreaming in class.", "You probably should not be ___ in class.", "The student ___ in his art class yesterday.", "___ up is hard today.", 
+	"I ___ The Eagles will win the Superbowl.", "All CCCS students ___ awesome!", "Anthony ___ school early because he was sick.",
 	
 	#Made sentences more complex by making the nouns and verbs somewhat harder to find/more vague, as well as subject, requires more thought
-	question3_array=["He ___ because he got a papercut.", "He ___ breakfast, even though he knew he should not have.", "The quarterback ___ the ball to his teammate quickly.", 
+	"He ___ because he got a papercut.", "He ___ breakfast, even though he knew he should not have.", "The quarterback ___ the ball to his teammate quickly.", 
 	"Dorothy prefers ___ in the morning instead of at night.", "He ___ his sister, even though she had been mean.", "Broccoli ___ a delicious food.",
 	"What is your favorite place to ___ about?", "Would you like to ___ to the zoo?", "Sunday is the best day for ___ a nap.", "I would like to ___ a hike today."]
 	
-	subject1_array =["The subject is 1. cat 2. bird 3. room ", "The subject is 1. John 2. school 3. is", "The subject is 1. now 2. you 3. right", 
+	subject_array =["The subject is 1. cat 2. bird 3. room ", "The subject is 1. John 2. school 3. is", "The subject is 1. now 2. you 3. right", 
 	"The subject is 1. prey 2. after 3. lions", "The subject is 1. I 2. house 3. your ", "The subject is 1. when 2. sad 3. people", 
 	"The subject is 1. cellphone 2. iPhone 3. a", "The subject is 1. the 2. player 3. basketball", "The subject is 1. Jay Z 2. Beyonce 3. stage", 
-	"The subject is 1. we 2. class 3. fun"]
+	"The subject is 1. we 2. class 3. fun",
 	#cat, John, you, lions, I, people, iPhone, player, Beyonce, we
+	#0, 0, 1, 2, 0, 2, 1, 1, 1, 0
 	
-	subject2_array =["The subject is 1. I 2. television 3. March Madness", "The subject is 1. paper 2. can 3. stressful", "The subject is 1. room 2. I 3. homework", 
+	"The subject is 1. I 2. television 3. March Madness", "The subject is 1. paper 2. can 3. stressful", "The subject is 1. room 2. I 3. homework", 
 	"The subject is 1. caught 2. he 3. class", "The subject is 1. you 2. class 3. should", "The subject is 1. art class 2. student 3. yesterday", 
-	"The subject is 1. is 2. hard 3. up", "The subject is 1. Superbowl 2. The Eagles 3. win", "The subject is 1. awesome 2. all 3. students", 
-	"The subject is 1. school 2. because 3. Anthony"]
-	#I, paper, I, he, you, student, waking, The Eagles, students, Anthony
+	"The subject is 1. today 2. hard 3. up", "The subject is 1. Superbowl 2. The Eagles 3. win", "The subject is 1. awesome 2. all 3. students", 
+	"The subject is 1. school 2. because 3. Anthony",
+	#I, paper, I, he, you, student, today, The Eagles, students, Anthony
+	#0, 0, 1, 1, 0, 1, 0, 1, 2, 2
 	
-	subject3_array =["The subject is 1. got 2. he 3. papercut", "The subject is 1. have 2. breakfast 3. he", "The subject is 1. quarterback 2. teammate 3. ball", 
+	"The subject is 1. got 2. he 3. papercut", "The subject is 1. have 2. breakfast 3. he", "The subject is 1. quarterback 2. teammate 3. ball", 
 	"The subject is 1. morning 2. Dorothy 3. night", "The subject is 1. sister 2. he 3. she",
-	"The subject is 1. food 2. delicious 3. broccoli", "The subject is 1. your 2. place 3. about", "The subject is 1. like 2. zoo 3. go",
-	"The subject is 1. nap 2. day 3. Sunday", "The subject is 1. today 2. hike 3. would"]
+	"The subject is 1. food 2. delicious 3. broccoli", "The subject is 1. your 2. place 3. about", "The subject is 1. you 2. zoo 3. go",
+	"The subject is 1. nap 2. day 3. Sunday", "The subject is 1. I 2. hike 3. would"]
 	#he, he, quarterback, Dorothy, he, broccoli, place, you, Sunday, I
+	#1, 2, 0, 1, 1, 2, 1, 0, 2, 0
 	
-	verb1_array = ["The correct form of the verb is 1. chase  2. chased  3. chasing", "The correct form of the verb is 1. walk 2. walking 3. walked",
+	verb_array = ["The correct form of the verb is 1. chase  2. chased  3. chasing", "The correct form of the verb is 1. walk 2. walking 3. walked",
 	"The correct form of the verb is 1. sit 2. sat 3. sitting", "The correct form of the verb is 1. ran 2. run 3. running", 
 	"The correct form of the verb is 1. come 2. came 3. coming", "The correct form of the verb is 1. cried 2. cry 3. crying", 
 	"The correct form of the verb is 1. is 2. has 3. are", "The correct form of the verb is 1. shoot 2. shooting 3. shot", 
-	"The correct form of the verb is 1. dancing 2. danced 3. dance", "The correct form of the verb is 1. learning 2. learn 3. learned"]
+	"The correct form of the verb is 1. dancing 2. danced 3. dance", "The correct form of the verb is 1. learning 2. learn 3. learned",
 	#chased, walking, sitting, run, come, cry, is, shot, danced, learn
+	#1, 1, 2, 1, 0, 1, 0, 2, 1, 1
 	
-	verb2_array=["The correct form of the verb is 1. watched 2. watch  3. watching ", "The correct form of the verb is 1. write 2. wrote 3. writing ", 
+	"The correct form of the verb is 1. watched 2. watch  3. watching ", "The correct form of the verb is 1. write 2. wrote 3. writing ", 
 	"The correct form of the verb is 1. bring 2. bringing 3. brought", "The correct form of the verb is 1. catching 2. catch 3. caught",
 	"The correct form of the verb is 1. talked 2. talking 3. talk", "The correct form of the verb is 1. draw 2. drew 3. drawing",
 	"The correct form of the verb is 1. wake 2. waking 3. woke", "The correct form of the verb is 1. swore 2. swear 3. swearing", 
-	"The correct form of the verb is 1. are 2. is 3. am", "The correct form of the verb is 1. learned 2. learn 3. learning"]
+	"The correct form of the verb is 1. are 2. is 3. am", "The correct form of the verb is 1. leave 2. left 3. leaving",
 	#watched, writing, brought, caught, talking, drew, waking, swear, are, left
+	#0, 2, 2, 2, 1, 1, 1, 1, 0, 1
 	
 	#Used non existant forms of words to increase difficulty, as well as more irregular verbs
-	verb3_array=["The correct form of the verb is 1. bleed 2. bled 3. bleeded", "The correct form of the verb is 1. skipped 2. skept 3. skip",
+	"The correct form of the verb is 1. bleed 2. bled 3. bleeded", "The correct form of the verb is 1. skipped 2. skept 3. skip",
 	"The correct form of the verb is 1. throw 2. through 3. threw", "The correct form of the verb is 1. shower 2. showering 3. showing", 
 	"The correct form of the verb is 1. forgave 2. forgived 3. forgiven", "The correct form of the verb is 1. is 2. are 3. am",
-	"The correct form of the verb is 1. think 2. thought 3. thinking", "The correct form of the verb is 1. go 2. goes 3. going", 
-	"The correct form of the verb is 1. having 2. haved 3. have", "The correct form of the verb is 1. take 2. took 3. taked"]
+	"The correct form of the verb is 1. think 2. thought 3. thinking", "The correct form of the verb is 1. going 2. goes 3. go", 
+	"The correct form of the verb is 1. had 2. having 3. have", "The correct form of the verb is 1. took 2. take 3. taked"]
 	#bled, skipped, threw, showering, forgave, is, think, go, having, take
+	#1, 0, 2, 1, 0, 0, 0, 2, 1, 1
 	
-	number1_array = ["How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 0 B. 1 C. 2", 
-	"How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 1 B. 2 C. 3",
-	"How many proper nouns are there A. 0 B. 1 C. 2", "How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 0 B. 1 C. 2"] 
-	# 3, 2, 0, 2, 1, 1, 2, 2, 3, 1 
+	number_array = ["How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 0 B. 1 C. 2", 
+	"How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 1 B. 2 C. 3",
+	"How many nouns are there A. 0 B. 1 C. 2", "How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 0 B. 1 C. 2",
+	#2, 1, 0, 1, 0, 0, 1, 2, 2, 1
 	
-	number2_array =["How many proper nouns are there A. 2 B. 0 C. 1", "How many proper nouns are there A. 0 B. 2 C. 1", "How many proper nouns are there A. 1 B. 2 C. 3", "How many proper nouns are there A. 2 B. 3 C. 1",
-	"How many proper nouns are there A. 4 B. 1 C. 2", "How many proper nouns are there A. 5 B. 1 C. 2", "How many proper nouns are there A. 0 B. 2 C. 1", "How many proper nouns are there A. 4 B. 3 C. 2",
-	"How many proper nouns are there A. 3 B. 2 C. 0", "How many proper nouns are there A. 0 B. 2 C. 3"]
-	#2, 1, 2, 1, 1, 2, 0, 2, 2, 2
+	"How many nouns are there A. 2 B. 0 C. 1", "How many nouns are there A. 0 B. 2 C. 1", "How many nouns are there A. 1 B. 2 C. 3", "How many nouns are there A. 2 B. 3 C. 1",
+	"How many nouns are there A. 4 B. 1 C. 2", "How many nouns are there A. 5 B. 1 C. 2", "How many nouns are there A. 0 B. 2 C. 1", "How many nouns are there A. 4 B. 3 C. 2",
+	"How many nouns are there A. 3 B. 2 C. 0", "How many nouns are there A. 0 B. 2 C. 3",
+	#0, 2, 1, 2, 1, 1, 1, 2, 1, 1
 	
-	number3_array=["How many proper nouns are there A. 5 B. 2 C. 1", "How many proper nouns are there A. 4 B. 1 C. 7", "How many proper nouns are there A. 3 B. 1 C. 6", "How many proper nouns are there A. 3 B. 4 C. 2",
-	"How many proper nouns are there A. 8 B. 1 C. 3", "How many proper nouns are there A. 5 B. 3 C. 2", "How many proper nouns are there A. 1 B. 6 C. 2", "How many proper nouns are there A. 5 B. 1 C. 2",
-	"How many proper nouns are there A. 0 B. 3 C. 1", "How many proper nouns are there A. 2 B. 1 C. 6"]
-	#1, 1, 3, 3, 1, 2, 1, 1, 3, 1
+	"How many nouns are there A. 5 B. 2 C. 1", "How many nouns are there A. 4 B. 1 C. 7", "How many nouns are there A. 3 B. 1 C. 6", "How many nouns are there A. 3 B. 4 C. 2",
+	"How many nouns are there A. 8 B. 1 C. 3", "How many nouns are there A. 5 B. 3 C. 2", "How many nouns are there A. 1 B. 6 C. 2", "How many nouns are there A. 5 B. 1 C. 2",
+	"How many nouns are there A. 0 B. 3 C. 1", "How many nouns are there A. 2 B. 1 C. 6"]
+	#2, 1, 0, 0, 1, 2, 0, 1, 1, 1
 
 #Possible hint: A noun is a person, place, or thing, while a proper noun is the more specific name of a person, place, or thing. 
-
+'''
 	verb1_answer_array = ["chased", "walking", "sitting", "run", "come", "cry", "is", "shot", "danced", "learn"]
 	verb2_answer_array = ["watched", "writing", "brought", "caught", "talking", "drew", "waking", "swear", "are", "left"]
 	verb3_answer_array = ["bled", "skipped", "threw", "showering", "forgave", "is", "think", "go", "having", "take"]
@@ -182,20 +188,26 @@ class Question(spyral.Scene):
 	subject1_answer_array = ["cat", "John", "you", "lions", "I", "people", "iPhone", "player", "Beyonce", "we"]
 	subject2_answer_array = ["I", "paper", "I", "he", "you", "student", "waking", "The Eagles", "students", "Anthony"]
 	subject3_answer_array = ["he", "he", "quarterback", "Dorothy", "he", "broccoli", "place", "you", "Sunday", "I"]
-
-	global x_array
-	x_array = [1,1]
+'''
+global x_array
+	x_array = [0, 0, 1, 2, 0, 2, 1, 1, 1, 0,
+	0, 0, 1, 1, 0, 1, 0, 1, 2, 2,
+	1, 2, 0, 1, 1, 2, 1, 0, 2, 0]
 	global y_array
-	y_array = [2,1]
+	y_array = [1, 1, 2, 1, 0, 1, 0, 2, 1, 1,
+	0, 2, 2, 2, 1, 1, 1, 1, 0, 1,
+	1, 0, 2, 1, 0, 0, 0, 2, 1, 1]
 	global z_array
-	z_array = [3,1]
+	z_array = [2, 1, 0, 1, 0, 0, 1, 2, 2, 1,
+	0, 2, 1, 2, 1, 1, 1, 2, 1, 1,
+	2, 1, 0, 0, 1, 2, 0, 1, 1, 1]	
 	sont = spyral.Font("/usr/share/fonts/truetype/freefont/FreeMono.ttf",30,(50,50,60))
 	#message4 = font.render(question_array[0]);
 	#message5 =sont.render(subject_array[0]);
 	#message6 = sont.render(verb_array[0]);
 	#message7 = sont.render(number_array[0]);
-	self.diff_form = selectDifficultyForm(self)
-	self.diff_form.focus()
+#	self.diff_form = selectDifficultyForm(self)
+#	self.diff_form.focus()
 
 	questionMessage(self,message4,(0),10)
 	questionMessage(self,message5,(0),575)
